190b795008c34aab580c94bb0951f3e73381018f
dark <dark>
2025-12-18 17:24:54 +0300

    Added new func

---
+++ create_envp_list.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muayna <muayna@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/13 13:31:34 by muayna            #+#    #+#             */
+/*   Updated: 2025/12/13 13:31:34 by muayna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "create_envp_list.h"
+
+static void	take_value(t_envp_list **list, char **envp)
+{
+	int			i;
+	t_envp_list	*temp;
+
+	temp = (*list);
+	i = 0;
+	while (envp[i])
+	{
+		temp->value = ft_strdup((ft_strchr(envp[i], '=') + 1));
+		temp = temp->next;
+		i++;
+	}
+	return ;
+}
+
+static void	take_key_name(t_envp_list **list, char **envp)
+{
+	int			i;
+	int			b;
+	t_envp_list	*tmp;
+
+	tmp = (*list);
+	i = 0;
+	while (envp[i])
+	{
+		tmp->id_name = ft_malloc(1, 0);
+		b = 0;
+		while (envp[i][b] != '=')
+		{
+			tmp->id_name[b] = envp[i][b];
+			b++;
+			tmp->id_name = realloc(tmp->id_name, (b + 1));
+		}
+		tmp->id_name[b] = '\0';
+		tmp->id = i;
+		tmp->next = ft_malloc(sizeof(t_envp_list), 0);
+		tmp = tmp->next;
+		tmp->next = NULL;
+		i++;
+	}
+	return ;
+}
+
+t_envp_list	*create_envp_list(char **envp)
+{
+	t_envp_list	*envp_list;
+
+	if (envp == NULL)
+		return (NULL);
+	envp_list = ft_malloc(sizeof(t_envp_list), 0);
+	take_key_name(&envp_list, envp);
+	take_value(&envp_list, envp);
+	return (envp_list);
+}
+
+/*
+int main(int argc, char *argv[], char **envp)
+{
+	(void)argc;
+	(void)argv;
+	t_envp_list *list = create_envp_list(envp);
+	while(list->next != NULL)
+	{
+		printf("%s=%s\n", list->id_name, list->value);
+		list = list->next;
+	}
+}*/
---
+++ get_user_value.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_user_value.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muayna <muayna@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/17 15:42:36 by muayna            #+#    #+#             */
+/*   Updated: 2025/12/17 15:42:36 by muayna           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_user_value.h"
+
+char *get_user_name(t_envp_list **list)
+{
+    char *user_name;
+
+    user_name = find_value_on_envp(list, "USER");
+    if(user_name == NULL)
+        return NULL;
+    user_name = ft_strjoin(COLOR_GREEN, user_name);
+    return (user_name);
+}
+
+static char *get_session_name(void)
+{
+    char *pc_name;
+    int pipefd[2];
+    pid_t pid;
+    char **cmd;
+
+    cmd = ft_split("hostname", ' ');
+    pc_name = NULL;
+    pipe(pipefd);
+    pid = fork();
+    if(pid == 0)
+    {
+        dup2(pipefd[1], 1);
+        close(pipefd[0]);
+        close(pipefd[1]);
+        execve("/bin/hostname", cmd, NULL);
+        exit(1);
+    }
+    else
+    {
+        free(cmd);
+        close(pipefd[1]);
+        pc_name = get_next_line(pipefd[0]);
+        close(pipefd[0]);
+    }
+    return pc_name;
+}
+
+char *get_pc_name(t_envp_list **list)
+{
+    char *pc_name;
+
+    pc_name = find_value_on_envp(list, "HOSTNAME");
+    if(pc_name == NULL)
+    {
+        pc_name = get_session_name();
+        if(pc_name == NULL)
+            NULL;
+        return pc_name;
+    }
+    else
+        return pc_name;
+    return NULL;
+}
+
+char *get_location(void)
+{
+    char *location;
+    char buf[1024];
+
+    getcwd(buf, 1024);
+    location = ft_strjoin(COLOR_PURPLE, buf);
+    return (location);
+}
+
+char *rl_header(t_envp_list **list, char **envp)
+{
+    char *prompt;
+    char *temp;
+
+    prompt = get_user_name(list);
+    temp = prompt;
+    prompt = ft_strjoin(prompt, "@");
+    free(temp);
+    temp = prompt;
+    prompt = ft_strjoin(prompt, get_pc_name(list));
+    free(temp);
+    temp = prompt;
+    prompt = cut_wrong_chracter(prompt);
+    free(temp);
+    temp = prompt;
+    prompt = ft_strjoin(prompt, "\033[1;37m:");
+    temp = prompt;
+    prompt = ft_strjoin(prompt, get_location());
+    free(temp);
+    temp = prompt;
+    prompt = ft_strjoin(prompt, "\033[1;37m$ ");
+    free(temp);
+    (void)envp;
+    return prompt;
+}
