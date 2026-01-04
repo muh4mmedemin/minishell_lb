4809d3b70909c550e2fc3d3ca284d50e25c21906
dark <dark>
2026-01-04 22:39:53 +0300

    ft_strcmp

--- string_edit.c
+++ string_edit.c
@@ -59,3 +59,19 @@ int	ft_arrlen(const char **arr)
 		i++;
 	return i;
 }
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		}
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
